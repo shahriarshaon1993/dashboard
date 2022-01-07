e8ec1b7813649064f97158b556452ffe211c7c48
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2022-01-07 14:50:52 +0600

    Create menu components

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -31,41 +31,7 @@
     </div>
     <div class="scrollbar-sidebar">
         <div class="app-sidebar__inner">
-            <ul class="vertical-nav-menu">
-                <li class="app-sidebar__heading">Menus</li>
-                <li>
-                    <a href="{{ route('admin.dashboard') }}" class="{{ Route::is('admin.dashboard') ? 'mm-active' : '' }}">
-                        <i class="metismenu-icon pe-7s-rocket"></i>
-                        Dashboard
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.pages.index') }}" class="{{ Request::is('admin/pages*') ? 'mm-active' : '' }}">
-                        <i class="metismenu-icon pe-7s-news-paper"></i>
-                        Pages
-                    </a>
-                </li>
-                <li class="app-sidebar__heading">Access Controll</li>
-                <li>
-                    <a href="{{ route('admin.users.index') }}" class="{{ Request::is('admin/users*') ? 'mm-active' : '' }}">
-                        <i class="metismenu-icon pe-7s-users"></i>
-                        Users
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.roles.index') }}" class="{{ Request::is('admin/roles*') ? 'mm-active' : '' }}">
-                        <i class="metismenu-icon pe-7s-check"></i>
-                        Roles
-                    </a>
-                </li>
-                <li class="app-sidebar__heading">System</li>
-                <li>
-                    <a href="{{ route('admin.backups.index') }}" class="{{ Request::is('admin/backups*') ? 'mm-active' : '' }}">
-                        <i class="metismenu-icon pe-7s-cloud"></i>
-                        Backups
-                    </a>
-                </li>
-            </ul>
+            <x-menu></x-menu>
         </div>
     </div>
 </div>
