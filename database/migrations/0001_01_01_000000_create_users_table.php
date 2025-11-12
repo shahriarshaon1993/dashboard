4344fdf6b4534c830f29a69a023d677aedd3b668
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-11-13 01:31:50 +0600

    feat: change to user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Enums\ActiveStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,9 +18,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 20)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('is_active', ActiveStatus::cases())
+                ->default(ActiveStatus::Active->value);
             $table->timestamps();
         });
 
--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -12,11 +12,11 @@ it('to array user', function (): void {
             'id',
             'name',
             'email',
+            'phone',
             'email_verified_at',
+            'is_active',
             'created_at',
             'updated_at',
             'two_factor_confirmed_at',
-            'phone',
-            'is_active',
         ]);
 });
